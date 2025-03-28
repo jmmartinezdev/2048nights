046d9b76938e045695c4524006b7862b52467d22
Chema martinez <jmmartinezdev@gmail.com>
2025-03-28 13:38:07 +0100

    Add swipe gesture to the board

--- 2048nights/Views/ContentView.swift
+++ 2048nights/Views/ContentView.swift
@@ -16,6 +16,24 @@ struct ContentView: View {
         
         BoardView(model: model.board)
             .padding()
+            .gesture(DragGesture(minimumDistance: 20, coordinateSpace: .global).onEnded({ value in
+                let horizontalMovement = value.translation.width
+                let verticalMovement = value.translation.height
+                    
+                if abs(horizontalMovement) > abs(verticalMovement) {
+                    if horizontalMovement < 0 {
+                        model.move(direction: .left)
+                    } else {
+                        model.move(direction: .right)
+                    }
+                } else {
+                    if horizontalMovement < 0 {
+                        model.move(direction: .up)
+                    } else {
+                        model.move(direction: .down)
+                    }
+                }
+            }))
         
         ArrowButtonsView { direction in
             model.move(direction: direction)
