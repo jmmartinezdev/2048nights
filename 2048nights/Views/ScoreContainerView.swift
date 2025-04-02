f9ed695caf476190489fd71d9f85b01a24ed776f
Chema martinez <jmmartinezdev@gmail.com>
2025-04-02 11:37:03 +0200

    Fix indentation in ScoreContainerView

--- 2048nights/Views/ScoreContainerView.swift
+++ 2048nights/Views/ScoreContainerView.swift
@@ -11,36 +11,48 @@ struct ScoreContainerView: View {
     @ObservedObject var model: ScoreModel
     @AppStorage("high_score") var highScore = 0
     @Environment(\.sizeCategory) var sizeCategory
-    
+
     var body: some View {
         if sizeCategory.isAccessibilityCategory {
             ScrollView(.horizontal) {
                 HStack {
-                    ScoreView(title: String(localized: "current_score"), score: model.score)
-                    ScoreView(title: String(localized: "high_score"), score: highScore)
+                    ScoreView(
+                        title: String(localized: "current_score"),
+                        score: model.score
+                    )
+                    ScoreView(
+                        title: String(localized: "high_score"),
+                        score: highScore
+                    )
                 }
                 .padding()
             }
         } else {
             HStack {
                 Spacer()
-                ScoreView(title: String(localized: "current_score"), score: model.score)
-                ScoreView(title: String(localized: "high_score"), score: highScore)
+                ScoreView(
+                    title: String(localized: "current_score"),
+                    score: model.score
+                )
+                ScoreView(
+                    title: String(localized: "high_score"),
+                    score: highScore
+                )
             }
             .padding()
         }
-        }
-        
+    }
+
 }
 
 #Preview {
     ScoreContainerView(model: ScoreModel())
 }
 
-fileprivate struct ScoreView: View {
+private struct ScoreView: View {
     let title: String
     let score: Int
-    
+
     var body: some View {
         VStack(content: {
             Text(title)
@@ -53,5 +65,5 @@ fileprivate struct ScoreView: View {
         .background(Color.accentColor)
         .cornerRadius(8)
     }
-    
+
 }
